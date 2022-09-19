45d657f7df789c450d2c4edb79b457a23fff4577
Ubuntu <ubuntu@ip-172-31-41-182.ap-southeast-1.compute.internal>
2022-09-19 06:17:22 +0000

    change dynamodb name

--- Day1/Ch05/terraform/terraform-backend/terraform-backend.tf
+++ Day1/Ch05/terraform/terraform-backend/terraform-backend.tf
@@ -11,7 +11,7 @@ resource "aws_s3_bucket" "test-s3-tf-state" {
 resource "aws_dynamodb_table" "test-ddb-tf-lock" {
 
   depends_on   = [aws_s3_bucket.test-s3-tf-state]
-  name         = "test-s3-ddb-tflock"
+  name         = "skt-user16-test-s3-ddb-tflock"
   billing_mode = "PAY_PER_REQUEST"
   hash_key     = "LockID"
 
